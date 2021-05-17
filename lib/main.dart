96420c7ec2309bae651d485218490d4e4ed375db
dev5576 <pronay@enzan-k.com>
2021-05-18 06:12:57 +0900

    commited all files

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hi'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Row(
+        children: <Widget>[
+          Expanded(flex: 1, child: Image.asset('assets/images/test1.jpg')),
+          Expanded(
+            flex: 3,
+            child: Container(
+                padding: EdgeInsets.all(30),
+                color: Colors.cyan,
+                child: Text('1')),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+                padding: EdgeInsets.all(30),
+                color: Colors.green,
+                child: Text('2')),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+                padding: EdgeInsets.all(30),
+                color: Colors.pink,
+                child: Text('3')),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Click!"),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
